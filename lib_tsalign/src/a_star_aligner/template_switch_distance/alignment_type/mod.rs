9e04897abb9ef54b7c1b1268b46945b132185c5d
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2024-10-11 10:18:33 +0300

    Refactor TS Identifier and AlignmentType into own modules.

---
+++ lib_tsalign/src/a_star_aligner/template_switch_distance/alignment_type/mod.rs
@@ -0,0 +1,71 @@
+use crate::a_star_aligner::alignment_result::IAlignmentType;
+
+use super::identifier::{TemplateSwitchPrimary, TemplateSwitchSecondary};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum AlignmentType {
+    /// The query contains a base that is missing from the reference.
+    Insertion,
+    /// The query is missing a base present in the reference.
+    Deletion,
+    /// The query contains a different base than the reference.
+    Substitution,
+    /// The query contains the same base as the reference.
+    Match,
+    /// A template switch entrance.
+    TemplateSwitchEntrance {
+        primary: TemplateSwitchPrimary,
+        secondary: TemplateSwitchSecondary,
+        first_offset: isize,
+    },
+    /// A template switch exit.
+    TemplateSwitchExit { length_difference: isize },
+    /// This node is the root node, hence it was not generated via alignment.
+    Root,
+    /// The root node of a secondary graph.
+    SecondaryRoot,
+    /// A reentry node into the primary graph, treated like a root.
+    PrimaryReentry,
+}
+
+impl IAlignmentType for AlignmentType {
+    fn is_repeatable(&self) -> bool {
+        match self {
+            Self::Insertion
+            | Self::Deletion
+            | Self::Substitution
+            | Self::Match
+            | Self::Root
+            | Self::SecondaryRoot
+            | Self::PrimaryReentry => true,
+            Self::TemplateSwitchEntrance { .. } | Self::TemplateSwitchExit { .. } => false,
+        }
+    }
+
+    fn is_repeated(&self, previous: &Self) -> bool {
+        match (self, previous) {
+            (
+                Self::TemplateSwitchEntrance {
+                    primary: primary_a,
+                    secondary: secondary_a,
+                    ..
+                },
+                Self::TemplateSwitchEntrance {
+                    primary: primary_b,
+                    secondary: secondary_b,
+                    ..
+                },
+            ) => primary_a == primary_b && secondary_a == secondary_b,
+            (Self::TemplateSwitchExit { .. }, Self::TemplateSwitchExit { .. }) => true,
+            (a, b) => a == b,
+        }
+    }
+
+    fn is_internal(&self) -> bool {
+        matches!(
+            self,
+            Self::Root | Self::SecondaryRoot | Self::PrimaryReentry
+        )
+    }
+}
---
+++ lib_tsalign/src/a_star_aligner/template_switch_distance/identifier/mod.rs
@@ -0,0 +1,329 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd, Hash)]
+pub enum Identifier {
+    Primary {
+        reference_index: usize,
+        query_index: usize,
+        gap_type: GapType,
+        /// Positive for left flank, negative for right flank.
+        flank_index: isize,
+    },
+    PrimaryReentry {
+        reference_index: usize,
+        query_index: usize,
+        gap_type: GapType,
+        /// Positive for left flank, negative for right flank.
+        flank_index: isize,
+    },
+    TemplateSwitchEntrance {
+        entrance_reference_index: usize,
+        entrance_query_index: usize,
+        template_switch_primary: TemplateSwitchPrimary,
+        template_switch_secondary: TemplateSwitchSecondary,
+        template_switch_first_offset: isize,
+    },
+    Secondary {
+        entrance_reference_index: usize,
+        entrance_query_index: usize,
+        template_switch_primary: TemplateSwitchPrimary,
+        template_switch_secondary: TemplateSwitchSecondary,
+        length: usize,
+        /// The index that does not jump.
+        primary_index: usize,
+        /// The index that jumps.
+        secondary_index: usize,
+        gap_type: GapType,
+    },
+    TemplateSwitchExit {
+        entrance_reference_index: usize,
+        entrance_query_index: usize,
+        template_switch_primary: TemplateSwitchPrimary,
+        template_switch_secondary: TemplateSwitchSecondary,
+        /// The index that does not jump.
+        primary_index: usize,
+        length_difference: isize,
+    },
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd, Hash)]
+pub enum GapType {
+    Insertion,
+    Deletion,
+    None,
+}
+
+/// The primary sequence is the sequence for which the template switch does not jump.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum TemplateSwitchPrimary {
+    Reference,
+    Query,
+}
+
+/// The secondary sequence is the sequence for which the template switch jumps.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum TemplateSwitchSecondary {
+    Reference,
+    Query,
+}
+
+impl Identifier {
+    pub const fn new_primary(
+        reference_index: usize,
+        query_index: usize,
+        flank_index: isize,
+        gap_type: GapType,
+    ) -> Self {
+        debug_assert!(reference_index != usize::MAX);
+        debug_assert!(query_index != usize::MAX);
+        debug_assert!(reference_index < isize::MAX as usize);
+        debug_assert!(query_index < isize::MAX as usize);
+
+        Self::Primary {
+            reference_index,
+            query_index,
+            flank_index,
+            gap_type,
+        }
+    }
+
+    pub fn generate_primary_diagonal_successor(self, flank_index: isize) -> Self {
+        match self {
+            Self::Primary {
+                reference_index,
+                query_index,
+                ..
+            }
+            | Self::PrimaryReentry {
+                reference_index,
+                query_index,
+                ..
+            } => {
+                debug_assert!(reference_index != usize::MAX);
+                debug_assert!(query_index != usize::MAX);
+                debug_assert!(reference_index < isize::MAX as usize);
+                debug_assert!(query_index < isize::MAX as usize);
+
+                Self::Primary {
+                    reference_index: reference_index + 1,
+                    query_index: query_index + 1,
+                    flank_index,
+                    gap_type: GapType::None,
+                }
+            }
+            other => unreachable!(
+                "Function is only called on primary identifiers, but this is: {other}."
+            ),
+        }
+    }
+
+    pub fn generate_primary_deletion_successor(self, flank_index: isize) -> Self {
+        match self {
+            Self::Primary {
+                reference_index,
+                query_index,
+                ..
+            }
+            | Self::PrimaryReentry {
+                reference_index,
+                query_index,
+                ..
+            } => {
+                debug_assert!(reference_index != usize::MAX);
+                debug_assert!(query_index != usize::MAX);
+                debug_assert!(reference_index < isize::MAX as usize);
+                debug_assert!(query_index < isize::MAX as usize);
+
+                Self::Primary {
+                    reference_index: reference_index + 1,
+                    query_index,
+                    flank_index,
+                    gap_type: GapType::Deletion,
+                }
+            }
+            other => unreachable!(
+                "Function is only called on primary identifiers, but this is: {other}."
+            ),
+        }
+    }
+
+    pub fn generate_primary_insertion_successor(self, flank_index: isize) -> Self {
+        match self {
+            Self::Primary {
+                reference_index,
+                query_index,
+                ..
+            }
+            | Self::PrimaryReentry {
+                reference_index,
+                query_index,
+                ..
+            } => {
+                debug_assert!(reference_index != usize::MAX);
+                debug_assert!(query_index != usize::MAX);
+                debug_assert!(reference_index < isize::MAX as usize);
+                debug_assert!(query_index < isize::MAX as usize);
+
+                Self::Primary {
+                    reference_index,
+                    query_index: query_index + 1,
+                    flank_index,
+                    gap_type: GapType::Insertion,
+                }
+            }
+            other => unreachable!(
+                "Function is only called on primary identifiers, but this is: {other}."
+            ),
+        }
+    }
+
+    pub fn generate_initial_template_switch_entrance_successors(
+        self,
+    ) -> impl Iterator<Item = Self> {
+        match self {
+            Identifier::Primary {
+                reference_index: entrance_reference_index,
+                query_index: entrance_query_index,
+                ..
+            }
+            | Identifier::PrimaryReentry {
+                reference_index: entrance_reference_index,
+                query_index: entrance_query_index,
+                ..
+            } => {
+                let template_switch_first_offset = 0;
+
+                [
+                    TemplateSwitchPrimary::Reference,
+                    TemplateSwitchPrimary::Reference,
+                    TemplateSwitchPrimary::Query,
+                    TemplateSwitchPrimary::Query,
+                ]
+                .into_iter()
+                .zip([
+                    TemplateSwitchSecondary::Reference,
+                    TemplateSwitchSecondary::Query,
+                    TemplateSwitchSecondary::Reference,
+                    TemplateSwitchSecondary::Query,
+                ])
+                .map(
+                    move |(template_switch_primary, template_switch_secondary)| {
+                        Identifier::TemplateSwitchEntrance {
+                            entrance_reference_index,
+                            entrance_query_index,
+                            template_switch_primary,
+                            template_switch_secondary,
+                            template_switch_first_offset,
+                        }
+                    },
+                )
+            }
+
+            other => unreachable!(
+                "Function is only called on primary identifiers, but this is: {other}."
+            ),
+        }
+    }
+
+    pub fn generate_secondary_diagonal_successor(self) -> Self {
+        match self {
+            Self::Secondary {
+                entrance_reference_index,
+                entrance_query_index,
+                template_switch_primary,
+                template_switch_secondary,
+                length,
+                primary_index,
+                secondary_index,
+                ..
+            } => Self::Secondary {
+                entrance_reference_index,
+                entrance_query_index,
+                template_switch_primary,
+                template_switch_secondary,
+                length: length + 1,
+                primary_index: primary_index + 1,
+                secondary_index: secondary_index - 1,
+                gap_type: GapType::None,
+            },
+            other => unreachable!(
+                "Function is only called on primary identifiers, but this is: {other}."
+            ),
+        }
+    }
+
+    /// The secondary contains a base missing in the primary.
+    pub fn generate_secondary_deletion_successor(self) -> Self {
+        match self {
+            Self::Secondary {
+                entrance_reference_index,
+                entrance_query_index,
+                template_switch_primary,
+                template_switch_secondary,
+                length,
+                primary_index,
+                secondary_index,
+                ..
+            } => Self::Secondary {
+                entrance_reference_index,
+                entrance_query_index,
+                template_switch_primary,
+                template_switch_secondary,
+                length,
+                primary_index,
+                secondary_index: secondary_index - 1,
+                gap_type: GapType::Deletion,
+            },
+            other => unreachable!(
+                "Function is only called on primary identifiers, but this is: {other}."
+            ),
+        }
+    }
+
+    /// The secondary misses a base present in the primary.
+    pub fn generate_secondary_insertion_successor(self) -> Self {
+        match self {
+            Self::Secondary {
+                entrance_reference_index,
+                entrance_query_index,
+                template_switch_primary,
+                template_switch_secondary,
+                length,
+                primary_index,
+                secondary_index,
+                ..
+            } => Self::Secondary {
+                entrance_reference_index,
+                entrance_query_index,
+                template_switch_primary,
+                template_switch_secondary,
+                length: length + 1,
+                primary_index: primary_index + 1,
+                secondary_index,
+                gap_type: GapType::Insertion,
+            },
+            other => unreachable!(
+                "Function is only called on primary identifiers, but this is: {other}."
+            ),
+        }
+    }
+
+    /// Returns the anti-diagonal for variants where it exists, or [`usize::MAX`](core::primitive::usize::MAX) otherwise.
+    pub const fn anti_diagonal(self) -> usize {
+        match self {
+            Self::Primary {
+                reference_index,
+                query_index,
+                ..
+            } => {
+                debug_assert!(reference_index != usize::MAX);
+                debug_assert!(query_index != usize::MAX);
+                debug_assert!(reference_index < isize::MAX as usize);
+                debug_assert!(query_index < isize::MAX as usize);
+
+                reference_index + query_index
+            }
+            _ => usize::MAX,
+        }
+    }
+}
