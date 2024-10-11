7e787c4573d94cdb3212f3fb37702b396ba81f61
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2024-10-11 11:00:27 +0300

    Add open and closed list to TS context.

--- lib_tsalign/src/a_star_aligner/template_switch_distance/context/mod.rs
+++ lib_tsalign/src/a_star_aligner/template_switch_distance/context/mod.rs
@@ -1,13 +1,31 @@
+use std::collections::HashMap;
+
+use binary_heap_plus::{BinaryHeap, MinComparator};
+
+use crate::a_star_aligner::template_switch_distance::Node;
+use crate::a_star_aligner::AlignmentGraphNode;
 use crate::config::TemplateSwitchConfig;
 
 use super::strategies::AlignmentStrategySelector;
 
 pub struct Context<Strategies: AlignmentStrategySelector> {
     pub config: TemplateSwitchConfig<Strategies::Alphabet>,
+
+    /// Can be used by strategies.
+    pub open_list: BinaryHeap<Node<Strategies>, MinComparator>,
+    /// Can be used by strategies.
+    pub closed_list: HashMap<
+        <Node<Strategies> as AlignmentGraphNode<Strategies::Alphabet>>::Identifier,
+        Node<Strategies>,
+    >,
 }
 
 impl<Strategies: AlignmentStrategySelector> Context<Strategies> {
     pub fn new(config: TemplateSwitchConfig<Strategies::Alphabet>) -> Self {
-        Self { config }
+        Self {
+            config,
+            open_list: BinaryHeap::new_min(),
+            closed_list: Default::default(),
+        }
     }
 }
