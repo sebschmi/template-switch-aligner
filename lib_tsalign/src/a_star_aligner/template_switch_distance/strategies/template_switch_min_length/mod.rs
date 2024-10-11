fbc321e5026c181ecc9cd20c74992af75a7eb665
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2024-10-11 12:00:42 +0300

    Implement cache hit branch for TS min length strategy.

--- lib_tsalign/src/a_star_aligner/template_switch_distance/strategies/template_switch_min_length/mod.rs
+++ lib_tsalign/src/a_star_aligner/template_switch_distance/strategies/template_switch_min_length/mod.rs
@@ -2,7 +2,10 @@ use std::collections::HashMap;
 
 use crate::{
     a_star_aligner::{
-        template_switch_distance::{Context, Node},
+        template_switch_distance::{
+            identifier::{GapType, TemplateSwitchPrimary, TemplateSwitchSecondary},
+            Context, Identifier, Node,
+        },
         AlignmentGraphNode,
     },
     costs::cost::Cost,
@@ -17,7 +20,7 @@ pub trait TemplateSwitchMinLengthStrategy: AlignmentStrategy {
     /// Takes the template switch entrance node and provides a lower bound for its costs depending on the minimum length of a template switch.
     /// The modified entrance node is returned in the iterator along with further nodes that were created while computing the lower bound.
     fn template_switch_min_length_lookahead<
-        Strategies: AlignmentStrategySelector,
+        Strategies: AlignmentStrategySelector<TemplateSwitchMinLength = Self>,
         SubsequenceType: compact_genome::interface::sequence::GenomeSequence<
                 Strategies::Alphabet,
                 SubsequenceType,
@@ -45,7 +48,7 @@ impl TemplateSwitchMinLengthStrategy for NoTemplateSwitchMinLengthStrategy {
     type Memory = ();
 
     fn template_switch_min_length_lookahead<
-        Strategies: AlignmentStrategySelector,
+        Strategies: AlignmentStrategySelector<TemplateSwitchMinLength = Self>,
         SubsequenceType: compact_genome::interface::sequence::GenomeSequence<
                 Strategies::Alphabet,
                 SubsequenceType,
@@ -65,11 +68,19 @@ impl TemplateSwitchMinLengthStrategy for NoTemplateSwitchMinLengthStrategy {
     }
 }
 
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+pub struct LookaheadMemoryKey {
+    template_switch_primary: TemplateSwitchPrimary,
+    template_switch_secondary: TemplateSwitchSecondary,
+    primary_index: usize,
+    secondary_index: usize,
+}
+
 impl TemplateSwitchMinLengthStrategy for LookaheadTemplateSwitchMinLengthStrategy {
-    type Memory = HashMap<(usize, usize), Cost>;
+    type Memory = HashMap<LookaheadMemoryKey, Cost>;
 
     fn template_switch_min_length_lookahead<
-        Strategies: AlignmentStrategySelector,
+        Strategies: AlignmentStrategySelector<TemplateSwitchMinLength = Self>,
         SubsequenceType: compact_genome::interface::sequence::GenomeSequence<
                 Strategies::Alphabet,
                 SubsequenceType,
@@ -85,8 +96,33 @@ impl TemplateSwitchMinLengthStrategy for LookaheadTemplateSwitchMinLengthStrateg
             Node<Strategies>,
         )>,
     ) -> impl IntoIterator<Item = Node<Strategies>> {
-        todo!();
-        []
+        let Identifier::Secondary {
+            template_switch_primary,
+            template_switch_secondary,
+            length: 0,
+            primary_index,
+            secondary_index,
+            gap_type: GapType::None,
+            ..
+        } = secondary_root_node.node_data.identifier
+        else {
+            unreachable!("Only called with a secondary root node.")
+        };
+
+        let memory_key = LookaheadMemoryKey {
+            template_switch_primary,
+            template_switch_secondary,
+            primary_index,
+            secondary_index,
+        };
+
+        if let Some(a_star_lower_bound) = context.template_switch_min_length_memory.get(&memory_key)
+        {
+            secondary_root_node.node_data.a_star_lower_bound = *a_star_lower_bound;
+            Some(secondary_root_node)
+        } else {
+            todo!()
+        }
     }
 }
 
