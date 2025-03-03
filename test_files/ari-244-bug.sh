c754ba2c2ee060e3abd4855c3698f43f9bdc1524
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2025-03-03 11:03:42 +0200

    Log test.

--- test_files/ari-244-bug.sh
+++ test_files/ari-244-bug.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cargo run -- -p test_files/twin_ari_email_244.fa --skip-characters - --alphabet dna-n -c test_files/config/experiments --ts-node-ord-strategy anti-diagonal --ts-min-length-strategy lookahead
+nohup cargo run -- -p test_files/twin_ari_email_244.fa --skip-characters - --alphabet dna-n -c test_files/config/experiments --ts-node-ord-strategy anti-diagonal --ts-min-length-strategy lookahead >ari-244-bug.log 2>&1
