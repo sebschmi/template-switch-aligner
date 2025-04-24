19db6556e16c7200b51c55e9ce3ae30c34cea651
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2025-04-24 13:02:58 +0300

    Add integration tests for cost and memory limits.

--- tsalign-tests/tests/integration.rs
+++ tsalign-tests/tests/integration.rs
@@ -12,3 +12,13 @@ fn test_align_default_cfg_twin() -> Result<()> {
 fn test_align_default_cfg_qr() -> Result<()> {
     run_in_repo_root("align -q test_files/query_a.fa -r test_files/reference_a.fa")
 }
+
+#[test]
+fn test_align_with_cost_limit() -> Result<()> {
+    run_in_repo_root("align -p test_files/twin_100_0.01.fa --cost-limit 0")
+}
+
+#[test]
+fn test_align_with_memory_limit() -> Result<()> {
+    run_in_repo_root("align -p test_files/twin_100_0.01.fa --memory-limit 1000")
+}
