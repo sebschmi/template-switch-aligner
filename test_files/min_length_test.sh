0399be07021f2ef1ab8001e17abf1032cb210b34
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2025-06-12 10:29:00 +0300

    Implement missing min length cases.

--- test_files/min_length_test.sh
+++ test_files/min_length_test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 cargo run -- align -p test_files/twin_min_length_test.fa --rq-ranges R40..75Q41..75
+cargo run -- align -p test_files/twin_min_length_test.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-price
+cargo run -- align -p test_files/twin_min_length_test.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-filter
