b0fb3b210f5998ea477b3d91a26d8fafa8fcecd0
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2025-06-12 10:42:33 +0300

    Fix primary index limit in min length strategy.

--- test_files/min_length_test.sh
+++ test_files/min_length_test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-cargo run -- align -p test_files/twin_min_length_test.fa --rq-ranges R40..75Q41..75
-cargo run -- align -p test_files/twin_min_length_test.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-price
-cargo run -- align -p test_files/twin_min_length_test.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-filter
+cargo run -- align -p test_files/twin_min_length_test1.fa --rq-ranges R40..75Q41..75
+cargo run -- align -p test_files/twin_min_length_test1.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-price
+cargo run -- align -p test_files/twin_min_length_test1.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-filter
+
+cargo run -- align -p test_files/twin_min_length_test2.fa --rq-ranges R40..75Q41..75
+cargo run -- align -p test_files/twin_min_length_test2.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-price
+cargo run -- align -p test_files/twin_min_length_test2.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-filter
