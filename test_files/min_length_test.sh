a7841faebea1bf92ae37f2b8f00eeaeb120efb9c
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2025-09-01 11:37:00 +0300

    Update test.

--- test_files/min_length_test.sh
+++ test_files/min_length_test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cargo run -- align -p test_files/twin_min_length_test1.fa --rq-ranges R40..75Q41..75
-cargo run -- align -p test_files/twin_min_length_test1.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-price
-cargo run -- align -p test_files/twin_min_length_test1.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-filter
+cargo run -- align -c test_files/config/experiments/ -p test_files/twin_min_length_test1.fa --rq-ranges R40..75Q41..75
+cargo run -- align -c test_files/config/experiments/ -p test_files/twin_min_length_test1.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-price
+cargo run -- align -c test_files/config/experiments/ -p test_files/twin_min_length_test1.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-filter
 
-cargo run -- align -p test_files/twin_min_length_test2.fa --rq-ranges R40..75Q41..75
-cargo run -- align -p test_files/twin_min_length_test2.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-price
-cargo run -- align -p test_files/twin_min_length_test2.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-filter
+cargo run -- align -c test_files/config/experiments/ -p test_files/twin_min_length_test2.fa --rq-ranges R40..75Q41..75
+cargo run -- align -c test_files/config/experiments/ -p test_files/twin_min_length_test2.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-price
+cargo run -- align -c test_files/config/experiments/ -p test_files/twin_min_length_test2.fa --rq-ranges R40..75Q41..75 --ts-min-length-strategy preprocess-filter
