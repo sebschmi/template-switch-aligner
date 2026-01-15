3c78dfb73bbc826f50ee959a8c888878dbd32d95
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2026-01-15 13:01:02 +0200

    Work.

--- lib_ts_chainalign/src/exact_chaining/gap_affine.rs
+++ lib_ts_chainalign/src/exact_chaining/gap_affine.rs
@@ -37,6 +37,27 @@ impl<'sequences, 'cost_table, 'rc_fn, Cost: AStarCost>
         }
     }
 
+    fn allow_direct_chaining(
+        &self,
+        start: AlignmentCoordinates,
+        end: AlignmentCoordinates,
+    ) -> bool {
+        start == self.sequences.primary_start() || end == self.sequences.primary_end()
+    }
+
+    fn allow_all_matches(&self, start: AlignmentCoordinates, end: AlignmentCoordinates) -> bool {
+        let minimum_primary_sequence_length =
+            (self.sequences.primary_end().primary_ordinate_a().unwrap()
+                - self.sequences.primary_start().primary_ordinate_a().unwrap())
+            .min(
+                self.sequences.primary_end().primary_ordinate_b().unwrap()
+                    - self.sequences.primary_start().primary_ordinate_b().unwrap(),
+            );
+        start == self.sequences.primary_start()
+            && end == self.sequences.primary_end()
+            && u32::try_from(minimum_primary_sequence_length).unwrap() <= self.max_match_run
+    }
+
     pub fn align(
         &mut self,
         start: AlignmentCoordinates,
@@ -48,28 +69,14 @@ impl<'sequences, 'cost_table, 'rc_fn, Cost: AStarCost>
             start.is_primary() && end.is_primary() || start.is_secondary() && end.is_secondary()
         );
 
-        let minimum_primary_sequence_length =
-            (self.sequences.primary_end().primary_ordinate_a().unwrap()
-                - self.sequences.primary_start().primary_ordinate_a().unwrap())
-            .min(
-                self.sequences.primary_end().primary_ordinate_b().unwrap()
-                    - self.sequences.primary_start().primary_ordinate_b().unwrap(),
-            );
-        let allow_direct_chaining =
-            start == self.sequences.primary_start() || end == self.sequences.primary_end();
-        let allow_all_matches = start == self.sequences.primary_start()
-            && end == self.sequences.primary_end()
-            && u32::try_from(minimum_primary_sequence_length).unwrap() <= self.max_match_run;
-
-        let enforce_non_match = true;
-
         let context = Context::new(
             self.cost_table,
             self.sequences,
             self.rc_fn,
             start,
             end,
-            enforce_non_match,
+            self.allow_direct_chaining(start, end),
+            self.allow_all_matches(start, end),
             self.max_match_run,
         );
         let mut a_star = AStar::new_with_buffers(context, self.a_star_buffers.take().unwrap());
@@ -86,10 +93,9 @@ impl<'sequences, 'cost_table, 'rc_fn, Cost: AStarCost>
         };
         a_star.search_until_with_target_policy(|_, node| node.cost > cost, true);
 
-        Self::fill_additional_targets(
+        self.fill_additional_targets(
             &a_star,
             start,
-            enforce_non_match,
             additional_primary_targets_output,
             additional_secondary_targets_output,
         );
@@ -108,7 +114,6 @@ impl<'sequences, 'cost_table, 'rc_fn, Cost: AStarCost>
         additional_primary_targets_output: &mut impl Extend<(PrimaryAnchor, Cost)>,
         additional_secondary_targets_output: &mut impl Extend<(SecondaryAnchor, Cost)>,
     ) {
-        let enforce_non_match = true;
         let end = self.sequences.end(start.ts_kind());
         debug_assert!(
             start.is_primary() && end.is_primary() || start.is_secondary() && end.is_secondary()
@@ -120,17 +125,17 @@ impl<'sequences, 'cost_table, 'rc_fn, Cost: AStarCost>
             self.rc_fn,
             start,
             end,
-            enforce_non_match,
+            true,
+            true,
             self.max_match_run,
         );
         let mut a_star = AStar::new_with_buffers(context, self.a_star_buffers.take().unwrap());
         a_star.initialise();
         a_star.search_until_with_target_policy(|_, node| node.cost > cost_limit, true);
 
-        Self::fill_additional_targets(
+        self.fill_additional_targets(
             &a_star,
             start,
-            enforce_non_match,
             additional_primary_targets_output,
             additional_secondary_targets_output,
         );
@@ -138,20 +143,36 @@ impl<'sequences, 'cost_table, 'rc_fn, Cost: AStarCost>
     }
 
     fn fill_additional_targets(
+        &self,
         a_star: &AStar<Context<Cost>>,
         start: AlignmentCoordinates,
-        enforce_non_match: bool,
         additional_primary_targets_output: &mut impl Extend<(PrimaryAnchor, Cost)>,
         additional_secondary_targets_output: &mut impl Extend<(SecondaryAnchor, Cost)>,
     ) {
         additional_primary_targets_output.extend(
             a_star
                 .iter_closed_nodes()
+                .filter(|node| node.identifier.coordinates.is_primary())
                 .filter(|node| {
-                    node.identifier.coordinates.is_primary()
-                        && ((node.identifier.has_non_match
-                            == (start != node.identifier.coordinates))
-                            || !enforce_non_match)
+                    start != node.identifier.coordinates
+                        || self.allow_direct_chaining(start, node.identifier.coordinates)
+                })
+                .filter(|node| {
+                    start == node.identifier.coordinates
+                        || node.identifier.has_non_match
+                        || self.allow_all_matches(start, node.identifier.coordinates)
+                })
+                .filter(|node| {
+                    // Filter duplicates.
+                    let mut pair_identifier = node.identifier;
+                    pair_identifier.has_non_match = !pair_identifier.has_non_match;
+                    a_star
+                        .closed_node(&pair_identifier)
+                        .map(|pair| {
+                            pair.cost > node.cost
+                                || (pair.cost == node.cost && node.identifier.has_non_match)
+                        })
+                        .unwrap_or(true)
                 })
                 .map(|node| {
                     (
@@ -163,11 +184,27 @@ impl<'sequences, 'cost_table, 'rc_fn, Cost: AStarCost>
         additional_secondary_targets_output.extend(
             a_star
                 .iter_closed_nodes()
+                .filter(|node| node.identifier.coordinates.is_secondary())
                 .filter(|node| {
-                    node.identifier.coordinates.is_secondary()
-                        && ((node.identifier.has_non_match
-                            == (start != node.identifier.coordinates))
-                            || !enforce_non_match)
+                    start != node.identifier.coordinates
+                        || self.allow_direct_chaining(start, node.identifier.coordinates)
+                })
+                .filter(|node| {
+                    start == node.identifier.coordinates
+                        || node.identifier.has_non_match
+                        || self.allow_all_matches(start, node.identifier.coordinates)
+                })
+                .filter(|node| {
+                    // Filter duplicates.
+                    let mut pair_identifier = node.identifier;
+                    pair_identifier.has_non_match = !pair_identifier.has_non_match;
+                    a_star
+                        .closed_node(&pair_identifier)
+                        .map(|pair| {
+                            pair.cost > node.cost
+                                || (pair.cost == node.cost && node.identifier.has_non_match)
+                        })
+                        .unwrap_or(true)
                 })
                 .map(|node| {
                     (
