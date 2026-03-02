f94a684cedb3db4a8d12dd507ec23baba6c1fed7
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2026-03-02 12:36:33 +0200

    Fix.

--- test_files/GNG12-AS1_200.sh
+++ test_files/GNG12-AS1_200.sh
@@ -2,11 +2,7 @@
 
 cargo build --release
 
-target/release/tsalign align -p test_files/GNG12-AS1_200.fa --alignment-method a-star-template-switch --ts-total-length-
-strategy none --skip-characters '-' --configuration-directory test_files/config/bench/ --alphabet dna --rq-ranges R196..219Q196..215 --ts-min-length-strategy none
-target/release/tsalign align -p test_files/GNG12-AS1_200.fa --alignment-method a-star-template-switch --ts-total-length-
-strategy none --skip-characters '-' --configuration-directory test_files/config/bench/ --alphabet dna --rq-ranges R196..219Q196..215 --ts-min-length-strategy lookahead
-target/release/tsalign align -p test_files/GNG12-AS1_200.fa --alignment-method a-star-template-switch --ts-total-length-
-strategy none --skip-characters '-' --configuration-directory test_files/config/bench/ --alphabet dna --rq-ranges R196..219Q196..215 --ts-min-length-strategy preprocess-price
-target/release/tsalign align -p test_files/GNG12-AS1_200.fa --alignment-method a-star-template-switch --ts-total-length-
-strategy none --skip-characters '-' --configuration-directory test_files/config/bench/ --alphabet dna --rq-ranges R196..219Q196..215 --ts-min-length-strategy preprocess-lookahead
+target/release/tsalign align -p test_files/GNG12-AS1_200.fa --alignment-method a-star-template-switch --ts-total-length-strategy none --skip-characters '-' --configuration-directory test_files/config/bench/ --alphabet dna --rq-ranges R196..219Q196..215 --ts-min-length-strategy none
+target/release/tsalign align -p test_files/GNG12-AS1_200.fa --alignment-method a-star-template-switch --ts-total-length-strategy none --skip-characters '-' --configuration-directory test_files/config/bench/ --alphabet dna --rq-ranges R196..219Q196..215 --ts-min-length-strategy lookahead
+target/release/tsalign align -p test_files/GNG12-AS1_200.fa --alignment-method a-star-template-switch --ts-total-length-strategy none --skip-characters '-' --configuration-directory test_files/config/bench/ --alphabet dna --rq-ranges R196..219Q196..215 --ts-min-length-strategy preprocess-price
+target/release/tsalign align -p test_files/GNG12-AS1_200.fa --alignment-method a-star-template-switch --ts-total-length-strategy none --skip-characters '-' --configuration-directory test_files/config/bench/ --alphabet dna --rq-ranges R196..219Q196..215 --ts-min-length-strategy preprocess-lookahead
