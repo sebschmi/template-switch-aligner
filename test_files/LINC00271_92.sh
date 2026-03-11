dc3e1a5f52bc95c9a801634fd87fe94f70e277d2
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2026-03-11 11:17:41 +0200

    Make script more efficient.

--- test_files/LINC00271_92.sh
+++ test_files/LINC00271_92.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 cargo build
-cargo build --release 
 
 if [ ! -f test_files/LINC00271_92.toml ]; then
+    cargo build --release 
     target/release/tsalign align -p test_files/LINC00271_92.fa -o test_files/LINC00271_92.toml --alignment-method a-star-template-switch --skip-characters 'N-' --alphabet dna -c test_files/config/bench --ts-node-ord-strategy anti-diagonal --ts-min-length-strategy preprocess-price -k 0 --max-chaining-successors 0 --max-exact-cost-function-cost 0 --chaining-closed-list special --chaining-open-list linear-heap --rq-ranges R196..227Q196..202
     target/release/tsalign align -p test_files/LINC00271_92.fa -o test_files/LINC00271_92.no_ts.toml --alignment-method a-star-template-switch --skip-characters 'N-' --alphabet dna -c test_files/config/bench --ts-node-ord-strategy anti-diagonal --ts-min-length-strategy preprocess-price -k 0 --max-chaining-successors 0 --max-exact-cost-function-cost 0 --chaining-closed-list special --chaining-open-list linear-heap --rq-ranges R196..227Q196..202 --no-ts
 fi
