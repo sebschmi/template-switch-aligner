6c5767d8f26d9c5af9ea92a52e6344f79d9de262
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2026-03-13 09:38:14 +0200

    Fix test.

--- test_files/STXBP5-AS1_99.sh
+++ test_files/STXBP5-AS1_99.sh
@@ -4,8 +4,8 @@ cargo build
 
 if [ ! -f test_files/STXBP5-AS1_99.toml ]; then
     cargo build --release
-    target/release/tsalign align -p test_files/STXBP5-AS1_99.fa -o test_files/STXBP5-AS1_99.toml --alignment-method a-star-template-switch --skip-characters 'N-' --alphabet dna -c test_files/config/bench --ts-node-ord-strategy anti-diagonal --ts-min-length-strategy preprocess-price -k 0 --max-chaining-successors 0 --max-exact-cost-function-cost 0 --chaining-closed-list special --chaining-open-list linear-heap --rq-ranges R196..227Q196..202
-    target/release/tsalign align -p test_files/STXBP5-AS1_99.fa -o test_files/STXBP5-AS1_99.no_ts.toml --alignment-method a-star-template-switch --skip-characters 'N-' --alphabet dna -c test_files/config/bench --ts-node-ord-strategy anti-diagonal --ts-min-length-strategy preprocess-price -k 0 --max-chaining-successors 0 --max-exact-cost-function-cost 0 --chaining-closed-list special --chaining-open-list linear-heap --rq-ranges R196..227Q196..202 --no-ts
+    target/release/tsalign align -p test_files/STXBP5-AS1_99.fa -o test_files/STXBP5-AS1_99.toml --alignment-method a-star-template-switch --skip-characters 'N-' --alphabet dna -c test_files/config/bench --ts-node-ord-strategy anti-diagonal --ts-min-length-strategy preprocess-price -k 0 --max-chaining-successors 0 --max-exact-cost-function-cost 0 --chaining-closed-list special --chaining-open-list linear-heap --rq-ranges R196..237Q196..204
+    target/release/tsalign align -p test_files/STXBP5-AS1_99.fa -o test_files/STXBP5-AS1_99.no_ts.toml --alignment-method a-star-template-switch --skip-characters 'N-' --alphabet dna -c test_files/config/bench --ts-node-ord-strategy anti-diagonal --ts-min-length-strategy preprocess-price -k 0 --max-chaining-successors 0 --max-exact-cost-function-cost 0 --chaining-closed-list special --chaining-open-list linear-heap --rq-ranges R196..237Q196..204 --no-ts
 fi
 
 target/debug/tsalign show -i test_files/STXBP5-AS1_99.toml -n test_files/STXBP5-AS1_99.no_ts.toml -ps test_files/STXBP5-AS1_99.svg --log-level trace
